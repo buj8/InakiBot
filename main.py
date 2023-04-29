2390c457061fddef7ed13fdece93c5200b4e86cc
buj8 <alvarobuj8@gmail.com>
2023-04-29 15:41:29 +0200

    aa

--- main.py
+++ main.py
@@ -6,21 +6,27 @@ from discord.ext import commands
 
 
 intents = discord.Intents.default()
-intents.message_content = True
+intents.members = True
+
 bot = commands.Bot(command_prefix='!', intents=intents)
+
+responses = {}
+with open('responses.txt', 'r') as f:
+    for line in f:
+        trigger, response = line.strip().split()
+        responses[trigger] = response
 
 
 @bot.event
 async def on_ready():
-    print(f"Logged in as {bot.user}")
+    print(f'{bot.user.name} has connected to Discord!')
+
 
 @bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
-
-@bot.command()
-async def hello(ctx):
-    await ctx.send("Choo choo! 🚅")
+async def respond(ctx):
+    response = responses.get(ctx.message.content)
+    if response:
+        await ctx.send(response)
 
 
 bot.run(os.environ["DISCORD_TOKEN"])
