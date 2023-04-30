f1f9c2f6ad24a329f60d63c6b7ce0992c7914112
buj8 <alvarobuj8@gmail.com>
2023-04-30 12:56:58 +0200

    audio

--- main.py
+++ main.py
@@ -3,11 +3,14 @@
 import os
 import discord
 from discord.ext import commands
+from discord.utils import get
 
 intents = discord.Intents.all()
 intents.members = True
 
 bot = commands.Bot(command_prefix="!", intents=intents)
+
+audio_files = {"stop inventing": "stop_inventing.mp3"}
 
 responses = {}
 with open("responses.txt", "r") as f:
@@ -26,6 +29,30 @@ async def on_message(message):
     response = responses.get(message.content)
     if response:
         await message.channel.send(response)
+
+
+@bot.command()
+async def play(ctx):
+    voice_channel = ctx.author.voice.channel
+    if not voice_channel:
+        await ctx.send("Métete en un canal, bobo.")
+        return
+
+    content = ctx.message.content.lower().split()[1]
+    filename = audio_files.get(content)
+    if not filename:
+        await ctx.send("Eso no existe.")
+        return
+
+    audio_source = discord.FFmpegPCMAudio(filename)
+    voice_client = get(bot.voice_clients, guild=ctx.guild)
+    if not voice_client:
+        voice_client = await voice_channel.connect()
+    else:
+        await voice_client.move_to(voice_channel)
+
+    if not voice_client.is_playing():
+        voice_client.play(audio_source, after=None)
 
 
 bot.run(os.environ["DISCORD_TOKEN"])
